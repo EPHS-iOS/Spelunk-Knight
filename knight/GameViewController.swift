41677e505bd40c7080133ee97a24ff1cca4a3437
kanishk <kanishkd04@gmail.com>
2022-02-23 13:12:37 -0600

    sstart menu

--- knight/GameViewController.swift
+++ knight/GameViewController.swift
@@ -18,7 +18,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = SKScene(fileNamed: "Menu") {
             
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFit
---
+++ knight/Menu.swift
@@ -0,0 +1,53 @@
+//
+//  Menu.swift
+//  knight
+//
+//  Created by 64004018 on 2/23/22.
+//
+
+//
+//  Menu.swift
+//  knight
+//
+//  Created by 64004018 on 2/23/22.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+import Foundation
+import AudioToolbox
+var startLabel: SKLabelNode?
+
+
+
+class Menu: SKScene {
+    override func didMove(to view: SKView){
+        startLabel = self.childNode(withName: "startLabel") as?SKLabelNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch: AnyObject in touches{
+            let pointOfTouch = touch.location(in: self)
+            
+            if ((startLabel?.contains(pointOfTouch)) != nil){
+                if let view = self.view {
+                    // Load the SKScene from 'GameScene.sks'
+                    if let scene = SKScene(fileNamed: "GameScene") {
+                        // Set the scale mode to scale to fit the window
+                        scene.scaleMode = .aspectFit
+                        
+                        // Present the scene
+                        view.presentScene(scene)
+                    }
+                    view.showsPhysics = false
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true //hi
+                }
+            }
+        }
+    }
+}
